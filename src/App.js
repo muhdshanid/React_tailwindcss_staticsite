576dd3d11aecba867eb1464cd7d59bd15fec9ad8
Muhammed Shanid <shanishanid91@gmail.com>
2022-11-18 17:32:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Analytics from "./Components/Analytics";
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Newsletter from "./Components/Newsletter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div className="lg:px-16">
+     <Navbar/>
+     </div>
+     <Hero/>
+     <div className="bg-white">
+     <Analytics/>
+     </div>
+     <div className="px-16">
+     <Newsletter/>
+     </div>
+     <div className="bg-white">
+     <Cards/>
+     </div>
+     <Footer/>
+     </div>
   );
 }
 
